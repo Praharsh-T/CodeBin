b918e349128df4a6c1649d415b32b65e201c4233
Praharsh-T <ppraharsht@gmail.com>
2024-07-14 18:33:19 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -10,25 +10,20 @@ const app = express();
 app.use(cors({ origin: true }));
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {})
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
-// Parse incoming JSON data (only once)
 app.use(express.json());
 
-// Mount paste routes
 app.use("/api/pastes", pasteRoutes);
 
-// Error handling middleware (optional, but recommended)
 app.use((err, req, res, next) => {
-  console.error(err.stack); // Log the error for debugging
-  res.status(500).json({ message: "Server Error" }); // Send a generic error message to the client
+  console.error(err.stack);
+  res.status(500).json({ message: "Server Error" });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
